57ae80a2e17384cab6446ea7a3358de808c5a77e
Julio Hernandez <ju010970@gmail.com>
2023-01-23 19:15:23 -0600

    Renderizacion de Todo agregada

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -8,7 +8,7 @@ const Filters = {
 
 
 const  state ={
-    todo: [
+    todos: [
         new Todo('Piedra del alma'),
         new Todo('Piedra del infinito'),
         new Todo('Piedra del tiempo'),
@@ -23,6 +23,81 @@ const initStore = () =>{
     console.log('InitStore')
 }
 
+
+const loadStroe = () => {
+    throw new Error('Not Implemented');
+}
+
+const getTodos = (filter = Filters.All)=>{
+    switch (filter) {
+        case Filters.All:
+            return [...state.todos]
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done)
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done)
+        default:
+            throw new Error(`Option ${filter} isn't valid`);
+    }
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = (description) =>{
+    if(!description) throw new Error('Description is required');
+    state.todos.push(new Todo(description));
+} 
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+//TODO: Optimizar con find slice e indexOf
+const toggleTodo = (todoId)=>{
+    state.todos = state.todos.map( todo =>{
+        if(todo.id === todoId){
+            todo.done = !todo.done;
+        }
+        return todo;
+    });
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const deleteTodo = (todoId)=>{    
+    state.todos = state.todos.filter( todo => todo.id !== todoId);
+
+}
+const deleteCompleted = ()=>{
+    state.todos = state.todos.filter( todo => todo.done);
+}
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFilter = ( newFilter = Filters.All) =>{
+
+    if(!Object.keys(Filters).includes(newFilter)) throw new Error(`The filter ${newFilter} is not valid`)
+    state.filter = newFilter
+}
+
+const getCurrentFilter = ()=>{
+    return state.filter.toString();
+}
+
+
 export default { 
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    getTodos,
     initStore,
+    loadStroe,
+    setFilter,
+    toggleTodo,
 }
--- src/todos/app.js
+++ src/todos/app.js
@@ -1,15 +1,24 @@
+import todoStore from '../store/todo.store';
 import html from './app.html?raw';
+import { renderTodos } from './usecases';
 
+const ElementIDs = {
+    TodoList: '.todo-list',
+}
 /**
  * 
  * @param {String} elementId 
  */
 export const App = (elementId) => {
-
+    const displayTodo = ()=>{
+        const todos = todoStore.getTodos( todoStore.getCurrentFilter());
+        renderTodos(ElementIDs.TodoList, todos);
+    }
     //Cuando se mande llamra la funcion
     (() => {
         const app = document.createElement('div');
         app.innerHTML = html;
         document.querySelector(elementId).append(app);
+        displayTodo();
     })();
 }
